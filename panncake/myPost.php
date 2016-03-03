533053ea2c2d7b818125abe813522bcae73cc20b
golconda <flagrans@hotmail.com>
2016-03-03 09:53:32 +0100

    Added one php-file

---
+++ panncake/myPost.php
@@ -0,0 +1,17 @@
+<?php
+define('DB_NAME', 'student');
+define('DB_USER', 'root');
+define('DB_PASSWORD','Forfattare1');
+define('DB_HOST', 'localhost');
+
+$link = mysql_connect(DB_HOST, DB_USER, DB_PASSWORD);
+
+if(!$link){
+    die('Kunde inte koppla'.mysql_error());
+}
+
+$db_selected = mysql_select_db(DB_NAME, $link);
+
+$value = $_POST['sname'];
+mysql_close();
+?>
